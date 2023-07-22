8bd1cce550eadfae2ebc4f9d8c9447d1bf31110d
Chandan Anandachari <anandachari.c@northeastern.edu>
2023-07-21 21:35:58 -0400

    Implemeneted the rest CRUD operations

--- src/main/java/com/udemyrest/restapi/rest/StudentRestController.java
+++ src/main/java/com/udemyrest/restapi/rest/StudentRestController.java
@@ -30,24 +30,4 @@ public class StudentRestController {
             throw new StudentNotFoundException("Student Id was not found"+studentId);
         return studentList.get(studentId);
     }
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception){
-        StudentErrorResponse studentErrorResponse=new StudentErrorResponse();
-
-        studentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        studentErrorResponse.setMessage(exception.getMessage());
-        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(studentErrorResponse,HttpStatus.NOT_FOUND);
-    }
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exception){
-        StudentErrorResponse studentErrorResponse=new StudentErrorResponse();
-
-        studentErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        studentErrorResponse.setMessage(exception.getMessage());
-        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(studentErrorResponse,HttpStatus.BAD_REQUEST);
-    }
 }
